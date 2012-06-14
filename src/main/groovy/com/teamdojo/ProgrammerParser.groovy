565100a15c86c63beb2ff01adfc02a291ebaeed6
Rob Purcell <borllecrup@me.com>
2012-06-14 16:16:15 +0100

    Process file input and create list of Programmers

--- src/main/groovy/com/teamdojo/ProgrammerParser.groovy
+++ src/main/groovy/com/teamdojo/ProgrammerParser.groovy
@@ -1,29 +1,31 @@
 package com.teamdojo
 
 class ProgrammerParser {
-	
+
 	def processInput(def xml) {
 		def records = new XmlSlurper().parseText(xml)
-		
+
 		createProgrammers(records)
 	}
-	
+
 	def createProgrammers(def records) {
-		//records.Programmer[0]
-		def programmer = new Programmer(name: records.Programmer[0].@name.text(),
-			skills: createSkills(records.Programmer[0].Skills),
-			recommendations: createRecommendations(records.Programmer[0].Recommendations))
-		return [programmer]
+		def programmers = []
+		for (programmer in records.Programmer) {
+			programmers.add(new Programmer(name: programmer.@name.text(),
+					skills: createSkills(programmer.Skills),
+					recommendations: createRecommendations(programmer.Recommendations)))
+		}
+		return programmers
 	}
-	
+
 	def createSkills(def skills) {
 		def result = []
 		for (def skill in skills.Skill) {
-			result.add(skill)	
+			result.add(skill)
 		}
 		return result
 	}
-	
+
 	def createRecommendations(def recommendations) {
 		def result = []
 		for (def recommendation in recommendations.Recommendation) {
@@ -31,5 +33,4 @@ class ProgrammerParser {
 		}
 		return result
 	}
-
 }
--- src/test/groovy/com/teamdojo/XMLReaderTest.groovy
+++ src/test/groovy/com/teamdojo/XMLReaderTest.groovy
@@ -3,6 +3,7 @@ package com.teamdojo;
 import static org.junit.Assert.*;
 import groovy.util.XmlSlurper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,6 +11,8 @@ import org.junit.Test;
  *
  */
 public class XMLReaderTest {
+
+	private ProgrammerParser parser
 
 	private static String PROGRAMMER = '''<Network>
 			<Programmer name='Rick'>
@@ -23,47 +26,54 @@ public class XMLReaderTest {
 </Programmer> 
 </Network>
 	'''
-	
+
 	private static String SKILLS = '''
 <Skills>
     <Skill>Java</Skill>
     <Skill>PHP</Skill>
   </Skills>
 '''
-	
+
+	@Before
+	public void setup() {
+		parser = new ProgrammerParser()
+	}
+
+
 	@Test
 	public void testSlurpXML() {
 		def records = new XmlSlurper().parseText(XMLReaderTest.PROGRAMMER)
 		assert records.size() == 1
 	}
-	
+
 	@Test
 	public void testCreateProgrammers() {
-		ProgrammerParser parser = new ProgrammerParser()
 		def result = parser.createProgrammers(new XmlSlurper().parseText(XMLReaderTest.PROGRAMMER))
 		assert result.size() == 1
 		assert result[0].hasProperty("name")
 		assert result[0].getName() == 'Rick'
 		assert result[0].getSkills() == ['Java', 'PHP']
 		assert result[0].getRecommendations() == ['Ed']
-	}	
-	
+	}
+
 	@Test
 	public void testCreateSkills() {
-		ProgrammerParser parser = new ProgrammerParser()
 		def result = new XmlSlurper().parseText(XMLReaderTest.SKILLS).Skill
 		assert result.size() == 2
-				
 	}
-	
+
 	@Test
 	public void testCreateSkillsImplementation() {
-		ProgrammerParser parser = new ProgrammerParser()
 		def result = parser.createSkills(new XmlSlurper().parseText(XMLReaderTest.PROGRAMMER).Programmer[0].Skills)
 		assert result.size() == 2
 		assert result[0].text() == 'Java'
 		assert result[1].text() == 'PHP'
 	}
-	
-	
+
+	@Test
+	public void testProcessInput() {
+		File xml = new File("src/main/resources/ProNet.xml")
+		def result = parser.processInput(xml.text)
+		assert result.size() == 10
+	}
 }
