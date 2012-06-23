23795c89e58f4c051cf3002ffd84419ad40a38c4
Vincent Riviere <vj_riviere@hotmail.com>
2012-06-23 10:27:01 +0100

    Adding PageRank algorithm

---
+++ src/main/groovy/com/teamdojo/kudos/PageRank.groovy
@@ -0,0 +1,133 @@
+package com.teamdojo.kudos
+
+import org.ejml.simple.SimpleMatrix
+
+/*
+ * Based on code by Nima Goodarzi
+ * Website: http://www.javadev.org
+ */
+class PageRank {
+
+	final def DAMPING_FACTOR = 0.85
+	
+	def outboundMap = [:]
+	def inboundMap = [:]
+
+	/*
+	 * Solve the equation of ax=b, which : a is the generated matrix based on
+	 * the parameter constants. x is the page ranks matrix. b is a n*1 matrix
+	 * which all the values are equal to the damping factor.
+	 */
+	def rank(String node) {
+		def params = []
+		generateParamList(node, params)
+		def a = new SimpleMatrix(generateMatrix(params))
+
+		def arrayB = new double[params.size()][1]
+		for (int i = 0; i < params.size(); i++) {
+			arrayB[i][0] = 1 - DAMPING_FACTOR
+		}
+
+		def b = new SimpleMatrix(arrayB)
+		// Solve the equation and get the page ranks
+		def x = a.solve(b)
+
+		def ind, cnt = 0
+		for (String currentNode : params) {
+			if (currentNode.equals(node))
+				ind = cnt
+			cnt++
+		}		
+		return round(x.get(ind,0), 2)
+	}
+
+	/*
+	 * This method generates the matrix of the linear equations. The generated
+	 * matrix is a n*n matrix where n is number of the related pages.
+	 */
+	def generateMatrix(params) {
+		def array = new double[params.size()][params.size()]
+
+		for (int i = 0; i < params.size(); i++) {
+			for (int j = 0; j < params.size(); j++) {
+				array[i][j] = getMultiFactor(params.get(i), params.get(j))
+			}
+		}
+		return array
+	}
+
+	/*
+	 * This method returns the constant of the given variable in the linear
+	 * equation.
+	 */
+	def getMultiFactor(sourceNode, linkNode) {
+		if (sourceNode == linkNode) {
+			return 1
+		}
+		else {
+			List<String> inboundNodes = getInboundNodes(sourceNode)
+			for (String inboundNode: inboundNodes) {
+				if (inboundNode.equals(linkNode)) {
+					return -1 * (DAMPING_FACTOR / getOutboundNodes(linkNode).size())
+				}
+			}
+		}
+		return 0
+	}
+
+	/*
+	 * This method returns list of the related nodes. This list is also the
+	 * parameters in the linear equation.
+	 */
+	def generateParamList(startingNode, params) {
+		// Add the starting node.
+		if (!params.contains(startingNode))
+			params.add(startingNode)
+
+		// Get list of the inbound node
+		def inboundNodes = getInboundNodes(startingNode)
+
+		// Recursive generate a list for each inboundNode found
+		for (String inboundNode : inboundNodes) {
+			if (!params.contains(inboundNode))
+				generateParamList(inboundNode, params)
+		}
+	}
+
+	def getInboundNodes(node) {
+		ensureNullSafeList(inboundMap.get(node))
+	}
+
+	def getOutboundNodes(node) {
+		ensureNullSafeList(outboundMap.get(node))
+	}
+	
+	def ensureNullSafeList(nodes) {
+		if (nodes == null) {
+			nodes = []
+		}
+		return nodes
+	}
+	
+	def addRecommendation(node, recommends) {
+		outboundMap.put(node, recommends);
+		for (String recommendedBy : recommends) {
+			def recommenders = getInboundNodes(recommendedBy)
+			if (recommenders.isEmpty()) {
+				inboundMap.put(recommendedBy, [node])
+			}
+			else {
+				recommenders << node
+				inboundMap.put(recommendedBy, recommenders)
+			}
+		}
+	}
+	
+	def round(d, decimalPlace) {
+		// see the Javadoc about why we use a String in the constructor
+		// http://docs.oracle.com/javase/6/docs/api/java/math/BigDecimal.html#BigDecimal(double)
+		def bd = new BigDecimal(Double.toString(d));
+		bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_DOWN);
+		return bd.doubleValue();
+	}
+}
---
+++ src/test/groovy/com/teamdojo/kudos/PageRankTest.groovy
@@ -0,0 +1,45 @@
+package com.teamdojo.kudos
+
+import org.junit.Before
+import org.junit.Test
+
+public class PageRankTest {
+	def PageRank pageRank
+	
+	@Before
+	public void setUp() {
+		pageRank = new PageRank();
+	}
+
+	@Test
+	public void thatANetworkWithTwoNodesLinkedToEachOtherBothHaveARankOfOne() {
+//		A <--> B
+		pageRank.addRecommendation("B", ["A"])
+		pageRank.addRecommendation("A", ["B"])
+		
+		assert pageRank.rank("A") == 1d
+		assert pageRank.rank("B") == 1d
+	}
+	
+	@Test
+	public void thatTheBelowSimpleNetworkReturnsTheCorrectRanks() {
+//		A --> B
+//		^   /
+//		|  /
+//		V L	
+//		C <-- D
+		
+		pageRank.addRecommendation("A", ["B","C"])
+		pageRank.addRecommendation("B", ["C"])
+		pageRank.addRecommendation("C", ["A"])
+		pageRank.addRecommendation("D", ["C"])
+		
+		assert pageRank.rank("A") == 1.49
+		assert pageRank.rank("B") == 0.78		
+//TODO: documentation expected 1.58: might be because EJML uses double instead of BigDecimal. Further investigation required.
+		assert pageRank.rank("C") == 1.59 
+		assert pageRank.rank("D") == 0.15
+
+	}
+
+}
